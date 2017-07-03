4067b4074f07b6209378398ea4c29e1e446c2856
bitguiders <bitguiders@bitguiders1-PC>
2017-07-03 01:24:41 -0500

    Main test run

--- print-center/src/main/java/com/solu/ps/print/Main.java
+++ print-center/src/main/java/com/solu/ps/print/Main.java
@@ -1,5 +1,6 @@
 package com.solu.ps.print;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -13,17 +14,24 @@ public class Main {
 		List<Document> pd2 = new ArrayList<Document>();
 
 		String fileName="fileName-";
-		for(int i=1;i<=5;i++){
+		for(int i=1;i<=500000;i++){
 			pd1.add(new Document(fileName+i+".pdf"));
 		}
-		for(int i=5;i<=10;i++){
+		for(int i=500000;i<=1000000;i++){
 			pd2.add(new Document(fileName+i+".xls"));
 		}
 		
+		LocalDateTime startTime = LocalDateTime.now();
 		HashSet<Document> pd3 = new PrintDocument().merge(pd1, pd2);
-		for(Document d: pd3 ){
+		LocalDateTime endTime = LocalDateTime.now();
+		
+		System.out.println("STARTED AT : "+startTime.getMinute()+":"+startTime.getSecond());
+		System.out.println("ENDED   AT : "+endTime.getMinute()+":"+endTime.getSecond());
+		System.out.println("Total Time Consumed ="+(endTime.getMinute() - startTime.getMinute())+":"+(endTime.getSecond() - startTime.getSecond()));
+		
+		/*for(Document d: pd3 ){
 			System.out.println(d.getFileName());
-		}
+		}*/
 	}
 
 }
